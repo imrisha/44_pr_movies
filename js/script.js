2d2a7e993ccc14ba107f765908ad75dd867c76bd
imrisha <imrisha@gmail.com>
2024-02-24 21:13:18 +0200

    48 add / delete in List

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => { // дождаться за
     const advers = document.querySelectorAll(".promo__adv img"), 
         poster = document.querySelector(".promo__bg"), // 3 один на странице
         filmsPrGen = poster.querySelector(".promo__genre"), // 2 жанр берем с постера
-        films = document.querySelector('.promo__interactive-list'), // 1
+        filmsList = document.querySelector('.promo__interactive-list'), // 1
        
         addForm = document.querySelector('form.add'),
         addInput = addForm.querySelector('.adding__input'),// value -что ввели
@@ -31,27 +31,6 @@ document.addEventListener('DOMContentLoaded', () => { // дождаться за
 //2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
 
    
-    addForm.addEventListener('submit', (event) => {
-        event.preventDefault(); // нет станд реакции, стран не перегр
-
-        let newFilm = addInput.value;// value -что ввели
-        const favorite = checkbox.ariaChecked;
-        if (newFilm) {
-            //2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
-            if (newFilm.length > 21) {
-                newFilm = `${newFilm.substring(0, 22)}...`//newFilm.slise(0,21) + '...'
-             }
-            movieDB.movies.push(newFilm) // add Film in movieDB
-            sortArr(movieDB.movies);
-            createMovieList(movieDB.movies, films);
-            addForm.reset(); // сбросить текст из формы --или event.target 
-        }
-    
-    }) 
-
-
-console.log(films.children);
-// f
 
 
     //ф-я удаления рекламы
@@ -59,9 +38,8 @@ console.log(films.children);
         advers.forEach(item => {item.remove();  }
     )};
      
-    deleteAdv (advers);
 
-    console.log( filmsPrGen );
+ 
     //filmsPr.forEach(item => (item.value == ))
     //2) Изменить жанр фильма, поменять "комедия" на "драма"
 
@@ -69,11 +47,10 @@ console.log(films.children);
         filmsPrGen.textContent = 'Драма'; // заменить со
         poster.style.backgroundImage = 'url("img/bg.jpg")' // ot html 
    }
-   makeChanges();
+
 
    // sort
    const sortArr = (arr) => {arr.sort()};
-   sortArr(movieDB.movies);
     
 
     // 4) Список фильмов на странице сформировать на основании данных из этого JS файла. Отсортировать их по алфавиту 
@@ -93,26 +70,73 @@ console.log(films.children);
                 <div class="delete"></div>
             </li>
         `;
-    })
+        })
+
+        //3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+        // пройти по детям навесить удалить реакцию на корзину
+        let n = parent.children.length;
+        console.log('ch' , n, parent.children);
+
+            for(let i=0; i < n; i++ ) {
+                let item = parent.children[i];            
+                const del = item.querySelector(".delete");
+                
+                del.addEventListener('click', () => {
+                    // find item.value in db and delete
+                    console.log('delete item:',  item);
+                //    item.remove();
+                //    movieDB.movies.splice(i, 1);
+                    films.splice(i, 1);
+                    console.log('new del',  movieDB.movies);  // вырезает i элемент 1 из массива или slice
+            //   movieDB.movies =  movieDB.movies.slice(0,i).concat(movieDB.movies.slice(i+1));
+
+                createMovieList(films, parent);
+                })            
+            }
+        // версия учителя
+/*         document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+
+                createMovieList(films, parent);
+            });
+        }); */
+
     }
 
-    createMovieList(movieDB.movies, films)
- //3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+deleteAdv (advers);
+sortArr(movieDB.movies);
+createMovieList(movieDB.movies, filmsList)
 
-console.log('ch' , films.children);
-movieDB.movies = movieDB.movies.slice(0,2) + movieDB.movies.slice(2+1);
+addForm.addEventListener('submit', (event) => {
+    event.preventDefault(); // нет станд реакции, стран не перегр
 
-films.children.forEach ((item, i) => {
-    const del = item.querySelector(".delete");
-    del.addEventListener('click', () => {
-        // find item.value in db and delete
-        item.remove();
-        movieDB.movies = movieDB.movies.slice(0,i) + movieDB.movies.slice(i+1);
-        console.log(item) 
-        createMovieList(movieDB.movies, films);
+    let newFilm = addInput.value;// value -что ввели
+    const favorite = checkbox.checked; //checkbox - значение
+    console.log ("favorite - ", favorite, newFilm, ' - new!!');
+    if (newFilm) {
+        //2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+        if (newFilm.length > 21) {
+            newFilm = `${newFilm.substring(0, 22)}...`//newFilm.slise(0,21) + '...'
+         }
+        movieDB.movies.push(newFilm) // add Film in movieDB
+        sortArr(movieDB.movies);
+        createMovieList(movieDB.movies, filmsList);
+        addForm.reset(); // сбросить текст из формы --или event.target 
+        if (favorite) {console.log (newFilm, ' - любимый!!')};
+    }
 
-    }) 
-});
+}) 
+
+
+/* console.log('ch' , films.children);
+console.log('фильмы ',  movieDB.movies); 
+console.log('slice 2 ',  movieDB.movies[2] , '\n___start', movieDB.movies.slice(0,2)); 
+console.log('TALE ',  movieDB.movies[2] ,  movieDB.movies.slice(2+1));
+
+console.log('ALL ',  movieDB.movies); */
 console.log(' -----\n', movieDB.movies); 
+createMovieList(movieDB.movies, filmsList);
 
-}) //совсем конец
+}); //совсем конец
